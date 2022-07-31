d9536c1aa9369fdacdcafa8ee7c393dd57051249
Toaster Fourtytoo <mike.troester@gmail.com>
2022-07-31 22:43:22 +0000

    fixed typo

--- docs/projects/2/script.js
+++ docs/projects/2/script.js
@@ -37,8 +37,8 @@ class Layer {
     if (this.x2 <= -this.width){
       this.x2 = this.width + this.x - this.speed;
     }
-    this.x = Math.floor(this.x = this.speed);
-    this.x2 = Math.floor(this.x2 = this.speed);
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
   }
   draw(){
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
