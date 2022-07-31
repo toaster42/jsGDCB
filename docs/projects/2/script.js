57a66cb5507bfa2a32372633e8cffbd47f1a46c7
Toaster Fourtytoo <mike.troester@gmail.com>
2022-07-31 22:46:26 +0000

    made parallax scroll rate better

--- docs/projects/2/script.js
+++ docs/projects/2/script.js
@@ -46,10 +46,10 @@ class Layer {
   }
 }
 
-const layer1 = new Layer(backgroundLayer1, 0.05);
-const layer2 = new Layer(backgroundLayer2, 0.1);
-const layer3 = new Layer(backgroundLayer3, 0.25);
-const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
 const layer5 = new Layer(backgroundLayer5, 1);
 
 const gameObjects = [layer1, layer2, layer3, layer4, layer5];
