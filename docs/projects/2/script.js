b74e4cccf29762642a13a2d8399f99d1a827488e
Toaster Fourtytoo <mike.troester@gmail.com>
2022-08-01 02:38:39 +0000

    fixed scroll algo

--- docs/projects/2/script.js
+++ docs/projects/2/script.js
@@ -32,7 +32,7 @@ class Layer {
     this.y = 0;
     this.width = 2400;
     this.height = 700;
-    this.x2 = this.width;
+    //this.x2 = this.width;
     this.image = image;
     this.speedModifier = speedModifier;
     this.speed = gameSpeed * this.speedModifier;
@@ -40,13 +40,14 @@ class Layer {
   update(){
     this.speed = gameSpeed * this.speedModifier;
     if (this.x <= -this.width){
-      this.x = this.width + this.x2 - this.speed;
+      this.x = 0;
     }
+    /*
     if (this.x2 <= -this.width){
       this.x2 = this.width + this.x - this.speed;
-    }
+    }*/
     this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
+    //this.x2 = Math.floor(this.x2 - this.speed);
   }
   draw(){
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
